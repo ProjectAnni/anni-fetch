2dee06e34d00800f48a9872e6f8a6e0eb24f92f1
Yesterday17 <t@yesterday17.cn>
2021-03-18 09:21:27 +0800

    working on the new RequestBuilder

--- src/client.rs
+++ src/client.rs
@@ -1,9 +1,13 @@
 use crate::io;
-use std::io::Read;
+use std::io::{Read, Cursor};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum ClientError {
+    #[error("invalid server status")]
+    InvalidServerStatus,
+    #[error("invalid response content type, expected {0}, got {1}")]
+    InvalidContentType(&'static str, String),
     #[error(transparent)]
     RequestError(#[from] ureq::Error),
     #[error(transparent)]
@@ -36,6 +40,8 @@ impl Client {
         Ok(PktIter::new(reader))
     }
 
+    /// Use [Client::request] instead
+    #[deprecated]
     pub fn command(&self, command: &str, capabilities: Option<&[(&str, Option<&[&str]>)]>, arguments: &[&str]) -> Result<impl Read + Send, ClientError> {
         let out = Vec::new();
         let mut cursor = std::io::Cursor::new(out);
@@ -68,20 +74,114 @@ impl Client {
             .into_reader())
     }
 
-    pub fn request(&self, command: &str, capabilities: Option<&[(&str, Option<&[&str]>)]>, arguments: &[&str]) -> Result<PktIter, ClientError> {
-        let reader = self.command(command, capabilities, arguments)?;
+    pub fn request(&self, body: Vec<u8>) -> Result<PktIter, ClientError> {
+        let response = self.client
+            .post(&format!("{}/git-upload-pack", &self.url))
+            .set("Git-Protocol", "version=2")
+            .set("Content-Type", "application/x-git-upload-pack-request")
+            .set("Accept", "application/x-git-upload-pack-result")
+            .send_bytes(&body)?;
+        if response.status() != 200 {
+            return Err(ClientError::InvalidServerStatus);
+        } else if let None = response.header("Content-Type") {
+            return Err(ClientError::InvalidContentType("application/x-git-upload-pack-result", "Nothing".to_owned()));
+        } else if let Some(v) = response.header("Content-Type") {
+            if v != "application/x-git-upload-pack-result" {
+                return Err(ClientError::InvalidContentType("application/x-git-upload-pack-result", v.to_owned()));
+            }
+        }
+        let reader = response.into_reader();
         Ok(PktIter::new(reader))
     }
 
     pub fn ls_ref(&self, prefix: &str) -> Result<String, ClientError> {
-        let mut result = self.command("ls-refs", None, &[&format!("ref-prefix {}", prefix)])?;
+        let mut result = self.command(
+            "ls-refs",
+            None,
+            &[
+                "peel",
+                "symrefs",
+                &format!("ref-prefix {}", prefix),
+            ],
+        )?;
         let (mut result, _) = io::read_pktline(&mut result)?;
         result.truncate(40);
         Ok(String::from_utf8(result)?)
     }
 
+    /// Use [RequestBuilder::want] with [Client::ls_ref] instead
+    #[deprecated]
     pub fn want_ref(&self, prefix: &str) -> Result<String, ClientError> {
         Ok(format!("want {}", self.ls_ref(prefix)?))
+    }
+}
+
+pub struct RequestBuilder {
+    inner: Cursor<Vec<u8>>,
+    delimeter_written: bool,
+    flush_written: bool,
+}
+
+impl RequestBuilder {
+    pub fn new(auto_packet: bool) -> Self {
+        let mut inner = Default::default();
+        io::write_pktline(&mut inner, "object-format=sha1").unwrap();
+        io::write_pktline(&mut inner, "agent=git/2.28.0").unwrap();
+        Self {
+            inner,
+            delimeter_written: auto_packet,
+            flush_written: auto_packet,
+        }
+    }
+
+    pub fn command(mut self, command: &str) -> Self {
+        io::write_pktline(&mut self.inner, &format!("command={}", command)).unwrap();
+        self
+    }
+
+    pub fn capability(mut self, name: &str, value: &[&str]) -> Self {
+        if value.len() != 0 {
+            io::write_pktline(&mut self.inner, &format!("{}={}", name, value.join(" "))).unwrap();
+        } else {
+            io::write_pktline(&mut self.inner, name).unwrap();
+        }
+        self
+    }
+
+    pub fn argument(mut self, arg: &str) -> Self {
+        if !self.delimeter_written {
+            self = self.packet(Message::Delimeter);
+            self.delimeter_written = true;
+        }
+
+        io::write_pktline(&mut self.inner, arg).unwrap();
+        self
+    }
+
+    pub fn packet(mut self, packet: Message) -> Self {
+        match packet {
+            Message::Flush => io::write_packet(&mut self.inner, 0).unwrap(),
+            Message::Delimeter => io::write_packet(&mut self.inner, 1).unwrap(),
+            Message::ResponseEnd => io::write_packet(&mut self.inner, 2).unwrap(),
+            _ => panic!("invalid packet type")
+        }
+        self
+    }
+
+    pub fn want(mut self, hash: &str) -> Self {
+        self.argument(&format!("want {}", hash))
+    }
+
+    pub fn have(mut self, hash: &str) -> Self {
+        self.argument(&format!("have {}", hash))
+    }
+
+    pub fn build(mut self) -> Vec<u8> {
+        if !self.flush_written {
+            self = self.packet(Message::Flush);
+            self.flush_written = true;
+        }
+        self.inner.into_inner()
     }
 }
 
@@ -183,6 +283,7 @@ mod tests {
     use crate::io::read_pktline;
     use crate::client::Message::*;
     use std::io::Cursor;
+    use crate::client::RequestBuilder;
 
     #[test]
     fn test_handshake() {
@@ -201,16 +302,11 @@ mod tests {
     }
 
     #[test]
-    fn test_ls_refs() {
-        let mut c = Client::new("https://github.com/project-anni/repo.git")
-            .command("ls-refs", None, &["ref-prefix HEAD"]).unwrap();
-        loop {
-            let (data, len) = read_pktline(&mut c).unwrap();
-            if len == 0 && data.len() == 0 {
-                break;
-            }
-            println!("{:?}", String::from_utf8_lossy(&data));
-        }
+    fn test_ls_ref() {
+        let hash = Client::new("https://github.com/project-anni/anni-fetch.git")
+            .ls_ref("refs/tags")
+            .unwrap();
+        assert_eq!(hash, "9192b5e5f2941fd76aa5a08043dc8aa6a31831a2");
     }
 
     #[test]
@@ -256,13 +352,16 @@ mod tests {
     #[test]
     fn test_fetch_iter() {
         let client = Client::new("https://github.com/project-anni/repo.git");
-        let iter = client.request("fetch", None, &[
-            "thin-pack",
-            "ofs-delta",
-            "deepen 1",
-            &client.want_ref("HEAD").expect("failed to get sha1 of HEAD"),
-            "done"
-        ]).unwrap();
+        let iter = client.request(
+            RequestBuilder::new(true)
+                .command("fetch")
+                .argument("thin-pack")
+                .argument("ofs-delta")
+                .argument("deepen 1")
+                .want(&client.ls_ref("HEAD").expect("failed to get sha1 of HEAD"))
+                .argument("done")
+                .build()
+        ).unwrap();
         let mut pack = Vec::new();
         for msg in iter {
             match msg {
